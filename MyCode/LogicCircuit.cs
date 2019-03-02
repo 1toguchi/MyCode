9775acae19ea354802c801cfcf01d857861cfc9b
ytksy1218 <ytksy1218@gmail.com>
2019-03-02 13:49:27 +0900

    Fix LogicalCircuit.cs

--- MyCode/LogicCircuit.cs
+++ MyCode/LogicCircuit.cs
@@ -12,11 +12,14 @@ namespace MyCode
         /// </summary>
         public class CombinationCircuit
         {
-            // [x] LogicGate
-            // [ ] DeCoder
+
+            // [ ] LogicGate
+            //TODO: Bit enzan
+                // [ ] TODO: Bitshift
             // [ ] EnCoder
             // [ ] MultiPlexer
-            // [x] Adder
+            // [ ] Adder
+                //TODO: completion 
             // [ ] Multiplier
 
             /// <summary>
@@ -26,10 +29,19 @@ namespace MyCode
             {
                 public static int NOT(int a) { return a == 0 ? 1 : 0; }
 
+                /// <summary>
+                /// bit演算におけるセットの役割。　0bX OR 1 で値をセットする。 0bX OR 0 だと値をセットしない。 
+                /// </summary>
                 public static int OR(int a, int b) { return (a == 1 || b == 1) ? 1 : 0; }
 
+                /// <summary>
+                /// bit演算におけるクリアの役割。　0bX AND 1 で値を残す。0bX AND 0 で値を残さない。
+                /// </summary>
                 public static int AND(int a, int b) { return (a == 1 && b == 1) ? 1 : 0; }
 
+                /// <summary>
+                /// bit演算における反転の役割。 0bX XOR 1 で値を反転する。 0bX XOR 0 で値を反転しない。
+                /// </summary>
                 public static int XOR(int a, int b) { return (a == b) ? 0 : 1; }
 
                 public static int NOR(int a, int b) { return (a == 0 && b == 0) ? 1 : 0; }
@@ -37,6 +49,23 @@ namespace MyCode
                 public static int NAND(int a, int b) { return (a == 1 && b == 1) ? 0 : 1; }   
             }
 
+            /// <summary>
+            /// ビット演算はあるまとまりのビット群に対して論理ゲートの処理を行うこと。
+            /// ここでは入力をintにしている。intを内部処理でstringに変換してintで吐き出す。
+            /// </summary>
+            class BitwiseOperation
+            {
+                //BitOR
+                //BitAND
+                //BitNOT
+                //BitXOR
+                //BitShift
+                    // LogicalRightShift
+                    // LogicalLeftShift
+                    // ArithmeticRightShift
+                    // ArithmeticLeftShift
+            }
+                
             /// <summary>
             /// 加算器 ＝ 二進数の加算を行うもの。全加算器と半加算器がある。
             /// 負の数の表現に２の補数(complement)を使ったり、減算は2の補数を加算することで実現できる。
@@ -73,6 +102,9 @@ namespace MyCode
             }
         }
 
+        /// <summary>
+        /// 順序回路 = 過去の入力と現在の入力で出力が決まる回路
+        /// </summary>
         public class SequentialCircuit
         {
             // [ ] FlipFlopCircuit == Register
